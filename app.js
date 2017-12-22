d2c6b1b63d1af5058dc38082a22655a0006c151a
preeme <roa.michael5@gmail.com>
2017-12-21 23:28:21 -0800

    add notes from terminal

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os')
-const notes = require('./notes')
+const notes = require('./notes');
+const _ = require('lodash');
+const yargs = require('yargs');
 
-var user = os.userInfo();
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+const argv = yargs.argv;
+var command = argv._[0];
+
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+}
+else if (command === 'list') {
+  notes.getAll();
+}
+else if (command === 'read'){
+  notes.readNote(argv.title);
+}
+else if (command === 'remove') {
+  notes.removeNote(argv.title);
+}
+else {
+  console.log('Command not recognized');
+}
--- notes.js
+++ notes.js
@@ -1,1 +1,42 @@
-console.log('starting notes.js')
+const fs = require('fs');
+
+var addNote = (title, body) => {
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(noteString);
+  }
+  catch(e) {
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+}
+
+var readNote = (title) => {
+  console.log('Getting note ', title);
+}
+
+var removeNote = (title) => {
+  console.log('Removed note')
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  readNote,
+  removeNote
+}
